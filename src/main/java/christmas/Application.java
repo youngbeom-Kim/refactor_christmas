545802fa73731f82600f310641ea56c4f2b0da23
youngbeom-Kim <abcd450196@gmail.com>
2023-11-30 20:04:03 +0900

    feat: 방문 날짜 메서드 실행

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,13 @@
 package christmas;
 
+import christmas.controller.PromotionController;
+import christmas.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PromotionController promotionController = new PromotionController();
+
+        promotionController.run();
     }
 }
---
+++ src/main/java/christmas/controller/PromotionController.java
@@ -0,0 +1,12 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+
+public class PromotionController {
+
+    public void run() {
+        InputView inputView = new InputView();
+        inputView.readDate();
+    }
+
+}
