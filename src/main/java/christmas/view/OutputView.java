cc5003432fb54fd18628dd0ca076f591549d0df7
youngbeom-Kim <abcd450196@gmail.com>
2023-12-05 19:37:38 +0900

    feat: OutputView 인사말 생성

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+import static christmas.constant.InfoMessage.GREETING_MESSAGE;
+
+public class OutputView {
+
+    public void printGreetingMessage() {
+        System.out.println(GREETING_MESSAGE.getMessage());
+    }
+}
