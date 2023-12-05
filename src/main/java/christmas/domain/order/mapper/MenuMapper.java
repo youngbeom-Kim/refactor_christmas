15f52a032e85a705011da3c02b6ada54cbd71932
youngbeom-Kim <abcd450196@gmail.com>
2023-12-05 20:33:36 +0900

    feat: 메뉴 Mapper 추가로 메뉴 이름 확인 기능 추가

---
+++ src/main/java/christmas/domain/order/mapper/MenuMapper.java
@@ -0,0 +1,25 @@
+package christmas.domain.order.mapper;
+
+import christmas.domain.constant.MenuType;
+import christmas.domain.constant.Menus;
+import christmas.domain.order.Menu;
+import christmas.domain.order.dto.MenuDto;
+import christmas.exception.IllegalArgumentExceptionType;
+
+public class MenuMapper {
+
+    public static Menu toDomain(MenuDto menuDto) {
+        String name = menuDto.name();
+        int count = menuDto.count();
+        Menus menus = convertMenuName(name);
+        return new Menu(menus, count);
+    }
+
+    private static Menus convertMenuName(String menuName) {
+        try {
+            return Menus.getByMenuName(menuName);
+        } catch (IllegalArgumentException e) {
+            throw IllegalArgumentExceptionType.INVALID_ORDER.getException();
+        }
+    }
+}
