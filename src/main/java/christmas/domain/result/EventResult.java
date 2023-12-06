b0d7e9dadab98d35d8b36e332c2e649685c8c719
youngbeom-Kim <abcd450196@gmail.com>
2023-12-06 17:57:15 +0900

    feat: 이벤트 결과 클래스 생성

--- src/main/java/christmas/domain/result/EventResult.java
+++ src/main/java/christmas/domain/result/EventResult.java
@@ -1,9 +1,40 @@
 package christmas.domain.result;
 
 import christmas.domain.benefit.Discount;
+import christmas.domain.order.Menu;
 
 import java.util.List;
 
 public class EventResult {
+    private final List<Discount> discounts;
+    private final List<Menu> gifts;
 
+    public EventResult(List<Discount> discounts, List<Menu> gifts) {
+        this.discounts = discounts;
+        this.gifts = gifts;
+    }
+
+    public List<Discount> getDiscounts() {
+        return discounts;
+    }
+
+    public List<Menu> getGifts() {
+        return gifts;
+    }
+
+    public int getTotalDiscount() {
+        return discounts.stream().mapToInt(Discount::getDiscountPrice).sum();
+    }
+
+    public int getTotalGiftsPrice() {
+        return gifts.stream().mapToInt(Menu::getTotalPrice).sum();
+    }
+
+    public int getTotalBenefitPrice() {
+        return getTotalGiftsPrice() - getTotalDiscount();
+    }
+
+    public int getTotalPriceAfterDiscount(int price) {
+        return price + getTotalDiscount();
+    }
 }
