f675d077554ec39c85780bab3b36f6ce86a6082e
youngbeom-Kim <abcd450196@gmail.com>
2023-12-06 17:45:45 +0900

    feat: 뱃지 클래스 생성

--- src/main/java/christmas/domain/constant/Badge.java
+++ src/main/java/christmas/domain/constant/Badge.java
@@ -9,7 +9,7 @@ public enum Badge {
     NO_BADGE("없음", 0),
     STAR("별", 5000),
     TREE("트리", 10000),
-    SANTA("산타", 20000)
+    SANTA("산타", 20000);
 
     private final String badge;
     private final int price;
