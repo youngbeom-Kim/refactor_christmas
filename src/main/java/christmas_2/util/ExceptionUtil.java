e08d0cf0f51caa8ed605ecc40c5514a6fbf6a0ec
youngbeom-Kim <abcd450196@gmail.com>
2023-12-07 17:53:54 +0900

    feat: IllegalArgumentException 예외 처리 발생

---
+++ src/main/java/christmas_2/util/ExceptionUtil.java
@@ -0,0 +1,10 @@
+package christmas_2.util;
+
+public class ExceptionUtil {
+
+    public static void throwInvalidValueException(String message) {
+
+        throw new IllegalArgumentException();
+    }
+
+}
