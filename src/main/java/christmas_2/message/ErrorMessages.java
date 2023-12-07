5c86fff778ce3ded5058a29013481aa3b93d0e7a
youngbeom-Kim <abcd450196@gmail.com>
2023-12-07 18:01:46 +0900

    feat: 에러 메세지 enum 상수화

---
+++ src/main/java/christmas_2/message/ErrorMessages.java
@@ -0,0 +1,20 @@
+package christmas_2.message;
+
+public enum ErrorMessages {
+
+    INVALID_DATE_INPUT_RE_ENTER_MESSAGE("유효하지 않은 날짜입니다. 다시 입력해주세요."),
+    INVALID_ORDER_INPUT_RE_ENTER_MESSAGE("유효하지 않은 주문입니다. 다시 입력해주세요."),
+    INVALID_NEGATIVE_VALUE_MESSAGE("음수값은 사용할 수 없습니다."),
+    INVALID_DIVISION_BY_ZERO_MESSAGE("0으로 나눌 수 없습니다.");
+
+    private final String message;
+    private static final String prefix = "[ERROR] ";
+
+    ErrorMessages(String message) {
+        this.message = prefix + message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
