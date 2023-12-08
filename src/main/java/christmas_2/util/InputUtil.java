531b925841013de7e6084fdcfa108c1177457601
youngbeom-Kim <abcd450196@gmail.com>
2023-12-08 19:32:55 +0900

    feat: Console API 이용하여 입력받기

---
+++ src/main/java/christmas_2/util/InputUtil.java
@@ -0,0 +1,10 @@
+package christmas_2.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputUtil {
+
+    public static String inputString() {
+        return Console.readLine();
+    }
+}
