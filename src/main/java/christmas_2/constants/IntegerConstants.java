3466685ad7f063a70aef8602994b97458b0f706d
youngbeom-Kim <abcd450196@gmail.com>
2023-12-09 21:11:35 +0900

    feat: 저장된 int 상수를 String으로도 출력할 수 있음

--- src/main/java/christmas_2/constants/IntegerConstants.java
+++ src/main/java/christmas_2/constants/IntegerConstants.java
@@ -2,6 +2,7 @@ package christmas_2.constants;
 
 public enum IntegerConstants {
     THIS_MONTH(12),
+    THIS_YEAR(2023),
     MINIMUM_EVENT_APPLY_PRICE(10000),
     MAX_MENU_COUNT(20),
     MIN_VISIT_DATE_RANGE(1),
@@ -15,5 +16,9 @@ public enum IntegerConstants {
 
     public int getValue() {
         return value;
+    }
+
+    public String toString() {
+        return String.valueOf(value);
     }
 }
