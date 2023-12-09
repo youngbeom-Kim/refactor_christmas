f65e7c1293f850bd546f477d5572378f5afb6cc0
youngbeom-Kim <abcd450196@gmail.com>
2023-12-09 22:15:03 +0900

    feat: 아이템이 20개 이상일 시 예외처리

--- src/main/java/christmas_2/domain/menu/Items.java
+++ src/main/java/christmas_2/domain/menu/Items.java
@@ -1,8 +1,11 @@
 package christmas_2.domain.menu;
 
+import christmas_2.util.ExceptionUtil;
 import org.mockito.internal.util.StringUtil;
 
 import java.util.HashMap;
+
+import static christmas_2.constants.IntegerConstants.MAX_MENU_ITEM_COUNT;
 
 public class Items {
 
@@ -21,5 +24,17 @@ public class Items {
 
     public HashMap<Item, ItemCount> getItems() {
         return new HashMap<>(items);
+    }
+
+    public int calcItemCounts() {
+        return items.values().stream()
+                .mapToInt(ItemCount::getCount)
+                .sum();
+    }
+
+    private void validateSumOfCounts() {
+        if (calcItemCounts() > MAX_MENU_ITEM_COUNT.getValue()) {
+            ExceptionUtil.throwInvalidValueException();
+        }
     }
 }
