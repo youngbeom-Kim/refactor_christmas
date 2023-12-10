6a15daaf2dbc0470d5ff3ae2f64bafbb57ec459a
youngbeom-Kim <abcd450196@gmail.com>
2023-12-10 20:02:16 +0900

    feat: 날짜가 어떤 starred인지 찾기, 날짜가 starred되어있는지 상태 확인

--- src/main/java/christmas_2/domain/event/Starred.java
+++ src/main/java/christmas_2/domain/event/Starred.java
@@ -3,6 +3,7 @@ package christmas_2.domain.event;
 import java.time.LocalDate;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static christmas_2.constants.IntegerConstants.THIS_MONTH;
 import static christmas_2.constants.IntegerConstants.THIS_YEAR;
@@ -17,9 +18,24 @@ public enum Starred {
         this.dates = toLocalDate(days);
     }
 
+    public static boolean isStarred(final LocalDate date) {
+        return YES.isContaining(date);
+    }
+
+    public static Starred findStarred(final LocalDate date) {
+        return Stream.of(Starred.values())
+                .filter(starred -> starred.isContaining(date))
+                .findFirst()
+                .orElse(NO);
+    }
+
     private Set<LocalDate> toLocalDate(final Set<Integer> days) {
         return days.stream()
                 .map(day -> LocalDate.of(THIS_YEAR.getValue(), THIS_MONTH.getValue(), day))
                 .collect(Collectors.toSet());
+    }
+
+    public boolean isContaining(final LocalDate date) {
+        return dates.contains(date);
     }
 }
