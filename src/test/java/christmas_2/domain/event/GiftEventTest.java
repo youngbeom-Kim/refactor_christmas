1f8ec3b3ed2072acef3820ae6a25f66fbfa01cc3
youngbeom-Kim <abcd450196@gmail.com>
2023-12-10 23:50:44 +0900

    test: 혜택의 discount 가격까지 검증

--- src/test/java/christmas_2/domain/event/GiftEventTest.java
+++ src/test/java/christmas_2/domain/event/GiftEventTest.java
@@ -11,21 +11,27 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class GiftEventTest {
     @ParameterizedTest
     @CsvSource({
-            "2023-12-10, 120000, 1", // 이벤트 기간 내, 최소 주문 금액 충족
-            "2024-01-01, 120000, 0", // 이벤트 기간 이후, 최소 주문 금액 충족
-            "2023-12-10, 119999, 0", // 이벤트 기간 내, 최소 주문 금액 미충족
-            "2023-11-30, 120000, 0"  // 이벤트 기간 이전, 최소 주문 금액 충족
+            "2023-12-10, 120000, 1, 25000", // 이벤트 기간 내, 최소 주문 금액 충족
+            "2023-12-26, 120000, 1, 25000", // 이벤트 기간 내, 최소 주문 금액 충족
+            "2023-12-10, 119999, 0, 0", // 이벤트 기간 내, 최소 주문 금액 미충족
+            "2024-01-26, 120000, 0, 0", // 이벤트 기간 이후, 최소 주문 금액 충족
+            "2023-11-30, 120000, 0, 0"  // 이벤트 기간 이전, 최소 주문 금액 충족
     })
-    void 증정_이벤트_계산(final String date, final int totalOrderPrice, final int expectedResult) {
-        // Given
+    void 증정_이벤트_계산(final String date,
+                   final int totalOrderPrice,
+                   final int expectedResult,
+                   final int expectedDiscountPrice) {
+        //Given
         final LocalDate testDate = LocalDate.parse(date);
         final Money orderPrice = Money.create(totalOrderPrice);
 
-        // When
-        Benefit benefit = GiftEvent.calculateBenefit(testDate, orderPrice);
-        Gifts gifts = benefit.getGifts();
+        //When
+        final Benefit benefit = GiftEvent.calculateBenefit(testDate, orderPrice);
+        final Gifts gifts = benefit.getGifts();
+        final Money money = benefit.getDiscountPrice();
 
-        // Then
+        //Then
         assertThat(gifts.size()).isEqualTo(expectedResult);
+        assertThat(money.getAmount()).isEqualTo(expectedDiscountPrice);
     }
 }
